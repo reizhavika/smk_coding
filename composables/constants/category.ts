0def895a0231ea54d4ffd5b0e7d6d53dfaaafcce
reizhavika <148943922+reizhavika@users.noreply.github.com>
2023-10-26 18:28:24 +0700

    Update category.ts

--- composables/constants/category.ts
+++ composables/constants/category.ts
@@ -1,1 +1,23 @@
 
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Kacamata",
+    },
+    {
+    id: 2,
+    name: "Dress",
+    },
+    {
+    id: 3,
+    name: "Sepatu",
+    },
+    {
+    id: 4,
+    name: "Celana"
+    }
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
