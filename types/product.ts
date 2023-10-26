226d3b4e09ce7748b4c4d2fef7a1c4fa4557f502
reizhavika <148943922+reizhavika@users.noreply.github.com>
2023-10-26 18:58:04 +0700

    Update product.ts

--- types/product.ts
+++ types/product.ts
@@ -1,1 +1,12 @@
 
+export interface Products {
+    id?: number,
+    name?: string,
+    category?: string,
+    price?: number,
+    image?: string,
+    color?: string,
+    isCart?: boolean,
+    description?: string,
+    quantity?: number,
+    }
